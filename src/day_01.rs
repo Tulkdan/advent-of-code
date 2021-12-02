ac0ec2342529c02ec7cfb8c8a9574b62080942cc
Pedro Correa <pedro8correa@gmail.com>
2021-12-02 16:03:53 -0300

    feat: first day

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,47 +1,50 @@
-pub fn find_two_values_that_sum_to_n(inputs: &Vec<u64>, n: u64) -> (u64, u64) {
-    let mut initial = 0;
-    let mut end = inputs.len() - 1;
+pub fn depth_increased_times(inputs: &Vec<u64>) -> u64 {
+    let mut times: u64 = 0;
+    let mut previous_value = inputs.first().unwrap();
 
-    let values = loop {
-        let sum = inputs[initial] + inputs[end];
-        if sum > n {
-            end -= 1;
-        } else if sum < n {
-            initial += 1;
-        } else {
-            break (inputs[initial], inputs[end]);
+    for depth in inputs.into_iter() {
+        if depth > previous_value {
+            times += 1;
         }
-    };
 
-    values
-}
-
-pub fn find_three_values_that_sum_to_n(inputs: &Vec<u64>, n: u64) -> Option<(u64, u64, u64)> {
-    for first_item in 0..inputs.len() {
-        for second_item in (first_item + 1)..inputs.len() {
-            let sum = inputs[first_item] + inputs[second_item];
-
-            if sum >= n {
-                break;
-            }
-
-            let val3 = n - sum;
-
-            if inputs[second_item + 1..]
-                .binary_search(&val3)
-                .is_ok()
-            {
-                return Some((inputs[first_item], val3, inputs[second_item]));
-            }
-        }
+        previous_value = depth;
     }
 
-    None
+    times
+}
+
+pub fn depth_window_increse_times(inputs: &Vec<u64>) -> u64 {
+    let mut times: u64 = 0;
+    let mut previous_value = inputs.iter()
+        .take(3)
+        .cloned()
+        .reduce(|acc, v| acc + v);
+
+    for i in 1..inputs.len() {
+        let scan_group = inputs.iter()
+            .skip(i)
+            .take(3);
+
+        if scan_group.len() < 3 {
+            break;
+        }
+
+        let actual_scan = scan_group.cloned()
+            .reduce(|acc, v| acc + v);
+
+        if actual_scan > previous_value {
+            times += 1;
+        }
+
+        previous_value = actual_scan;
+    }
+
+    times
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{find_two_values_that_sum_to_n, find_three_values_that_sum_to_n};
+    use super::{depth_increased_times, depth_window_increse_times};
     use std::fs::File;
     use std::io::{BufReader, BufRead};
 
@@ -49,52 +52,48 @@ mod tests {
         let input = File::open("input/day_01").unwrap();
         let buffered = BufReader::new(input);
 
-        let mut content: Vec<u64> = buffered.lines()
+        let content: Vec<u64> = buffered.lines()
             .into_iter()
             .filter_map(Result::ok)
             .map(|number| number.parse::<u64>().unwrap())
             .collect();
-
-        content.sort();
 
         content
     }
 
     #[test]
     fn sample_input_first_challenge() {
-        let sample_input = vec![299, 366, 675, 979, 1456, 1721];
+        let sample_input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-        let values =  find_two_values_that_sum_to_n(&sample_input, 2020);
+        let value =  depth_increased_times(&sample_input);
 
-        assert_eq!((299, 1721), values);
+        assert_eq!(7, value);
     }
 
     #[test]
     fn first_challenge() {
         let input = open_test_file();
 
-        let values =  find_two_values_that_sum_to_n(&input, 2020);
+        let value =  depth_increased_times(&input);
 
-        assert_eq!((337, 1683), values);
-        assert_eq!(567171, values.0 * values.1);
+        assert_eq!(1400, value);
     }
 
     #[test]
     fn sample_input_second_challenge() {
-        let sample_input = vec![299, 366, 675, 979, 1456, 1721];
+        let sample_input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-        let values =  find_three_values_that_sum_to_n(&sample_input, 2020).unwrap();
+        let values =  depth_window_increse_times(&sample_input);
 
-        assert_eq!((366, 979, 675), values);
+        assert_eq!(5, values);
     }
 
     #[test]
     fn second_challenge() {
         let input = open_test_file();
 
-        let values =  find_three_values_that_sum_to_n(&input, 2020).unwrap();
+        let values =  depth_window_increse_times(&input);
 
-        assert_eq!((281, 877, 862), values);
-        assert_eq!(212428694, values.0 * values.1 * values.2);
+        assert_eq!(1429, values);
     }
 }
