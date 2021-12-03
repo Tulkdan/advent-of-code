e52eaf2034930e59f324328f25a0ffb467587020
Pedro Correa <pedro8correa@gmail.com>
2021-12-02 21:04:17 -0300

    feat: day 02

--- src/day_02.rs
+++ src/day_02.rs
@@ -2,71 +2,125 @@ extern crate regex;
 
 use regex::Regex;
 
-pub fn password_validator(passwords: &Vec<String>) -> usize {
-    let re = Regex::new(r"(\d+)-(\d+) (\w): (.*)").unwrap();
-    let mut valid_count = 0;
+pub fn calculate_position(commands: &Vec<String>) -> u64 {
+    let mut depth: u64 = 0;
+    let mut horizontal: u64 = 0;
 
-    for password in passwords {
-        let cap = re.captures(password).unwrap();
-        let min = cap[1].parse::<usize>().unwrap();
-        let max = cap[2].parse::<usize>().unwrap();
-        let character = cap[3].chars().collect::<Vec<char>>()[0];
-        let pass = &cap[4];
+    for command in commands.into_iter() {
+        let re = Regex::new(r"(\w+) (\d+)").unwrap();
 
-        let mut count = 0;
-        for pw_character in pass.bytes() {
-            if character as u8 == pw_character {
-                count += 1;
-            }
-        }
+        let cap = re.captures(command).unwrap();
 
-        if count >= min && count <= max {
-            valid_count += 1;
-        }
+        let action = &cap[1];
+        let value = cap[2].parse::<u64>().unwrap();
+
+        match action {
+            "forward" => { horizontal += value; },
+            "down" => { depth += value; },
+            "up" => { depth -= value; },
+            _ => {}
+        };
     }
 
-    valid_count
+    depth * horizontal
+}
+
+pub fn calculate_position_with_aim(commands: &Vec<String>) -> u64 {
+    let mut depth: u64 = 0;
+    let mut horizontal: u64 = 0;
+    let mut aim: u64 = 0;
+
+    for command in commands.into_iter() {
+        let re = Regex::new(r"(\w+) (\d+)").unwrap();
+
+        let cap = re.captures(command).unwrap();
+
+        let action = &cap[1];
+        let value = cap[2].parse::<u64>().unwrap();
+
+        match action {
+            "forward" => {
+                horizontal += value;
+
+                if aim > 0 {
+                    depth += aim * value;
+                }
+            },
+            "down" => {
+                aim += value;
+            },
+            "up" => {
+                aim -= value;
+            },
+            _ => {}
+        };
+    }
+
+    depth * horizontal
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{password_validator};
+    use super::{calculate_position, calculate_position_with_aim};
     use std::fs::File;
     use std::io::{BufReader, BufRead};
+
+    fn get_sample_input() -> Vec<String> {
+        vec![
+            "forward 5".to_owned(),
+            "down 5".to_owned(),
+            "forward 8".to_owned(),
+            "up 3".to_owned(),
+            "down 8".to_owned(),
+            "forward 2".to_owned()
+        ]
+    }
 
     fn open_test_file() -> Vec<String> {
         let input = File::open("input/day_02").unwrap();
         let buffered = BufReader::new(input);
 
-        let mut content: Vec<String> = buffered.lines()
+        let content: Vec<String> = buffered.lines()
             .into_iter()
             .filter_map(Result::ok)
             .collect();
-
-        content.sort();
 
         content
     }
 
     #[test]
     fn sample_input_first_challenge() {
-        let sample_input = vec![
-            "1-3 a: abcde".to_owned(),
-            "1-3 b: cdefg".to_owned(),
-            "2-9 c: ccccccccc".to_owned()
-        ];
+        let sample_input = get_sample_input();
 
-        let values = password_validator(&sample_input);
+        let values = calculate_position(&sample_input);
 
-        assert_eq!(2, values);
+        assert_eq!(150, values);
     }
 
     #[test]
     fn first_challenge() {
         let input = open_test_file();
 
-        let values = password_validator(&input);
+        let values = calculate_position(&input);
 
-        assert_eq!(467, values);
+        assert_eq!(1654760, values);
+    }
+
+    #[test]
+    fn sample_input_second_challenge() {
+        let sample_input = get_sample_input();
+
+        let values = calculate_position_with_aim(&sample_input);
+
+        assert_eq!(900, values);
+    }
+
+    #[test]
+    fn second_challenge() {
+        let input = open_test_file();
+
+        let values = calculate_position_with_aim(&input);
+
+        assert_eq!(1956047400, values);
     }
 }
